b8c4cf4556daac6f048c94e5e9338e7f289fad49
Ante Šućur <Ante.Sucur@internationalvalueservices.com>
2023-02-25 21:04:36 +0100

    Create users.json mock data

--- src/main/java/com/huddleproject/bookstore/model/Order.java
+++ src/main/java/com/huddleproject/bookstore/model/Order.java
@@ -1,6 +1,9 @@
 package com.huddleproject.bookstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -8,6 +11,9 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
   private List<Book> orderedBooks;
   private BigDecimal basePrice;
--- src/main/java/com/huddleproject/bookstore/model/User.java
+++ src/main/java/com/huddleproject/bookstore/model/User.java
@@ -1,5 +1,14 @@
 package com.huddleproject.bookstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   private Long id;
   private String username;
